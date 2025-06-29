d5191ac9d67beff6acad846e4eefc411af499fd4
ksimanov <ksimanov@craftus.com>
2025-06-29 21:04:05 +0200

    chore: update configuration files for consistency and formatting improvements

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-	preset: "ts-jest",
-	testEnvironment: "node",
-	roots: ["<rootDir>/packages"],
-	testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/*.(test|spec).+(ts|tsx|js)"],
-	transform: {
-		"^.+\\.(ts|tsx)$": "ts-jest",
-	},
-	collectCoverageFrom: [
-		"packages/**/*.{ts,tsx}",
-		"!packages/**/*.d.ts",
-		"!packages/**/node_modules/**",
-	],
-	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-	passWithNoTests: true,
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/packages"],
+  testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/*.(test|spec).+(ts|tsx|js)"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  collectCoverageFrom: [
+    "packages/**/*.{ts,tsx}",
+    "!packages/**/*.d.ts",
+    "!packages/**/node_modules/**",
+  ],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  passWithNoTests: true,
 };
