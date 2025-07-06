70b747dd3a2d22eccc2a9dc23f964ce20f224e01
ksimanov <ksimanov@craftus.com>
2025-07-06 13:02:45 +0200

    fix(examples): remove unnecessary globals configuration from Jest setup

--- examples/note-server/jest.config.ts
+++ examples/note-server/jest.config.ts
@@ -4,11 +4,6 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
   transform: {
     '^.+\\.ts$': ['ts-jest', { useESM: true }],
   },
